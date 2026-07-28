5e490e5a9ec2a174cb8af3bbf4b336487de0a13d
Isha-Sisodia <ishasisodia77@gmail.com>
2026-07-28 14:42:22 +0530

    Update 38.two_sum.py

--- 38.two_sum.py
+++ 38.two_sum.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=eeudwo2i
+# Brute force (Accepted) 
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
@@ -6,4 +7,13 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i,j]
 
+# Optimised (Accepted)
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i in range(len(nums)):
+            needed = target - nums[i]
+            if needed in seen:
+                return [seen[needed],i]
+            seen[nums[i]] = i
         
